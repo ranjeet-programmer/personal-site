712312e20e22723b886e7bf8ae752ba60ab4f7bb
ranjeet-programmer <ranjeethingeofficial@gmail.com>
2021-06-29 06:54:01 +0000

    about us page completed

--- src/components/About.js
+++ src/components/About.js
@@ -2,34 +2,61 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "../App.css";
 import revenge from "../images/revenge.jpg";
+import { Bar } from "react-chartjs-2";
+
 const About = () => {
+  const state = {
+    labels: ["C++", "Javascript", "React JS", "PHP", "Core Java"],
+    datasets: [
+      {
+        label: "Skills",
+        backgroundColor: "rgba(75,192,192,1)",
+        borderColor: "rgba(0,0,0,1)",
+        borderWidth: 2,
+        data: [30, 30, 40, 50, 56],
+      },
+    ],
+  };
+
   return (
     <div>
-      <p>About</p>
-
       <div className='about_design container'>
         <div className='row '>
-          <div className='col-md-5 about_1'>
-            <p>Hello </p>
-            <img src={revenge} alt='about_image' className='about_img_design' />
+          <div className='col-md-6 about_1'>
+            <div className='octagonWrap'>
+              <div className='octagon'>
+                <img
+                  src={revenge}
+                  alt='about_image'
+                  className='about_img_design'
+                />
+              </div>
+            </div>
+
             <h3 className='image_text'>Who's This Guy ?</h3>
             <p className='image_text2'>
               I am currently pursuing B.E In Information Technology from PCCOE
               pune . I am always intrested to learn new things regarding tech
               stuff. I am a competitive programmer and i do competitive
-              programming beacuse i like to do things out of comfort zone.
+              programming beacuse i like to solve problems..
             </p>
           </div>
 
-          <div className='col-md-7 about_2'>
-            <p>About us</p>
-            <p className='about_text'>
-              I am currently pursuing B.E In Information Technology from PCCOE
-              pune .<br></br> I am always intrested to learn new things
-              regarding tech stuff. <br></br>I am a competitive programmer and i
-              do competitive programming beacuse i like to do things out of
-              comfort zone.
-            </p>
+          <div className='col-md-6 about_2'>
+            <Bar
+              data={state}
+              options={{
+                title: {
+                  display: true,
+                  text: "Average skills",
+                  fontSize: 20,
+                },
+                legend: {
+                  display: true,
+                  position: "right",
+                },
+              }}
+            />
           </div>
         </div>
       </div>
